4e1dfd9037a906edead78a7b7d5765a1fb526032
Dirk-Jan Wassink <dirk.jan.wassink@gmail.com>
2016-12-13 21:47:20 +0100

    Publish to NPM

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,37 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
-var minifyCss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 gulp.task('js', function() {
     return gulp.src('./src/js/*.js')
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('uglify-js', ['js'], function() {
+    return gulp.src('./build/*.js')
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest('./build'))
 });
 
 gulp.task('sass', function () {
-    return sass('./src/sass/*.sass')
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./dist'));
+    return gulp.src('./src/sass/*.sass')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./build'));
+});
 
+gulp.task('clean-css', ['sass'], function() {
+    return gulp.src('./build/*.css')
+      .pipe(cleanCss({compatibility: 'ie8'}))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./build'))
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./src/sass/*.sass', ['sass']);
-    gulp.watch('./src/js/*.js', ['sass']);
+    gulp.watch('./src/sass/*.sass', ['sass', 'clean-css']);
+    gulp.watch('./src/js/*.js', ['js', 'uglify-js']);
 });
 
-gulp.task('default', ['sass', 'js']);
+gulp.task('default', ['sass', 'clean-css', 'js', 'uglify-js']);
